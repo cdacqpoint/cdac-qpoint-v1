0bab53a9630ec2c1bad3f5bd33af3d1a290a3171
Sai Krishnan S <sai.saikrishnan@gmail.com>
2020-01-14 19:12:43 +0530

    sample autosuggestion - xi

---
+++ src/views/Area55.js
@@ -0,0 +1,160 @@
+import React from "react";
+import {
+    Container,
+    Row,
+    Col,
+    Card
+} from "shards-react";
+import Autosuggest from 'react-autosuggest';
+import "../assets/react-autosuggest.css"
+
+const languages = [
+    {
+        name: 'How do I read / convert an InputStream into a String in Java?',
+        year: 1972
+    },
+    {
+        name: 'C#',
+        year: 2000
+    },
+    {
+        name: 'C++',
+        year: 1983
+    },
+    {
+        name: 'Clojure',
+        year: 2007
+    },
+    {
+        name: 'Elm',
+        year: 2012
+    },
+    {
+        name: 'Go',
+        year: 2009
+    },
+    {
+        name: 'Haskell',
+        year: 1990
+    },
+    {
+        name: 'Java',
+        year: 1995
+    },
+    {
+        name: 'Javascript',
+        year: 1995
+    },
+    {
+        name: 'Perl',
+        year: 1987
+    },
+    {
+        name: 'PHP',
+        year: 1995
+    },
+    {
+        name: 'Python',
+        year: 1991
+    },
+    {
+        name: 'Ruby',
+        year: 1995
+    },
+    {
+        name: 'Scala',
+        year: 2003
+    }
+];
+const renderInputComponent = inputProps => (
+    <div className="ml-3 input-group input-group-seamless">
+        <div className="input-group-prepend">
+            <span className="input-group-text">
+                <i className="material-icons">search</i>
+            </span>
+        </div>
+        <input  {...inputProps} placeholder="Search for something..." className="navbar-search form-control" />
+    </div>
+);
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+    const escapedValue = escapeRegexCharacters(value.trim());
+
+    if (escapedValue === '') {
+        return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+    return (
+        <span class="media">
+            <div class="media-body">
+                <h6 class="mt-0">{suggestion.name}</h6>
+                <small>Asked 2 days ago, Active 2 days ago</small>
+            </div>
+        </span>
+
+    );
+}
+
+export default class Example extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            value: '',
+            suggestions: []
+        };
+    }
+
+    onChange = (event, { newValue, method }) => {
+        this.setState({
+            value: newValue
+        });
+    };
+
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: getSuggestions(value)
+        });
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
+    render() {
+        const { value, suggestions } = this.state;
+        const inputProps = {
+            placeholder: "Type 'c'",
+            value,
+            onChange: this.onChange
+        };
+
+        return (
+            <Autosuggest
+                suggestions={suggestions}
+                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                getSuggestionValue={getSuggestionValue}
+                renderInputComponent={renderInputComponent}
+                renderSuggestion={renderSuggestion}
+                inputProps={inputProps} />
+        );
+    }
+}
+

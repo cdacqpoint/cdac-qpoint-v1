951290d0c1c8176d5ac48b1c479d19ebc5fd39b0
lia-bilquis <bilquisalisha@gmail.com>
2020-01-18 21:58:54 +0530

    Categories architecture changes

--- src/_constants/app.constants.js
+++ src/_constants/app.constants.js
@@ -4,24 +4,24 @@ export default {
     SHOW_LOADER: "SHOW_LOADER",//Questions are loading, probably ajax call
 
     FETCH_COURSE_TAGS: "FETCH_COURSE_TAGS",// Fetch Course tags
-    FETCH_CATEGORIES: "FETCH_CATEGORIES",// Fetch Course tags
+    FETCH_CATEGORIES: "FETCH_CATEGORIES",// Fetch Course categories
     FETCH_QUESTIONS: "FETCH_QUESTIONS",// Fetch Questions
     FETCH_VARIABLES: "FETCH_VARIABLES",// Fetch Questions
 
     FETCH_COURSE_TAGS_ERROR: "FETCH_COURSE_TAGS_ERROR",// Error in Fetch Course tags
-    FETCH_CATEGORIES_ERROR: "FETCH_CATEGORIES_ERROR",// Error in Fetch Course tags
+    FETCH_CATEGORIES_ERROR: "FETCH_CATEGORIES_ERROR",// Error in Fetch Course categories
     FETCH_QUESTIONS_ERROR: "FETCH_QUESTIONS_ERROR",// Error in Fetch Questions
     FETCH_VARIABLES_ERROR: "FETCH_VARIABLES_ERROR",// Error in Fetch Questions
 
     RECIEVE_COURSE_TAGS: "RECIEVE_COURSE_TAGS",// Recieve Course tags
-    RECIEVE_CATEGORIES: "RECIEVE_CATEGORIES",// Recieve Course tags
+    RECIEVE_CATEGORIES: "RECIEVE_CATEGORIES",// Recieve Course categories
     RECIEVE_QUESTIONS: "RECIEVE_QUESTIONS",// Recieve Questions
     RECIEVE_VARIABLES: "RECIEVE_VARIABLES",// Recieve Questions
 
     CHANGE_FILTER: "CHANGE_FILTER",//Filter questions  
     CHANGE_QUESTION_PER_PAGE: "CHANGE_QUESTION_PER_PAGE",//Questions per page
     CHANGE_COURSE_TAGS: "CHANGE_COURSE_TAGS",//Course tags
-    CHANGE_CATEGORIES: "CHANGE_CATEGORIES",//Course tags
+    CHANGE_CATEGORIES: "CHANGE_CATEGORIES",//Course categories
 
     PAGENATE_QUESTIONS: "PAGENATE_QUESTIONS",//Change pagenation    
     CREATE_QUESTION: "CREATE_QUESTION", // Create question
---
+++ src/_dummy/categories.js
@@ -0,0 +1,127 @@
+import { random } from "../_helpers/random";
+export default {
+    init: () => {
+        localStorage.setItem('tags', JSON.stringify([
+            {
+                _id: random(25),
+                name: "JAVA",
+                url: "../categories/java/questions",
+                totalCount: 2000,
+                todaysCount: 150,
+                weekCount: 400
+            },
+            {
+                _id: random(25),
+                name: "Angular",
+                url: "../categories/angular/questions",
+                totalCount: 222,
+                todaysCount: 10,
+                weekCount: 150
+            },
+            {
+                _id: random(25),
+                name: "PHP",
+                url: "../categories/php/questions",
+                totalCount: 103,
+                todaysCount: 10,
+                weekCount: 10
+            },
+            {
+                _id: random(25),
+                name: "asp.NET",
+                url: "../categories/asp.net/questions",
+                totalCount: 93,
+                todaysCount: 10,
+                weekCount: 20
+            },
+            {
+                _id: random(25),
+                name: "C++",
+                url: "../categories/c++/questions",
+                totalCount: 10,
+                todaysCount: 0,
+                weekCount: 0
+            },
+            {
+                _id: random(25),
+                nname: "C#",
+                url: "../categories/c#/questions",
+                totalCount: 10,
+                todaysCount: 0,
+                weekCount: 0
+            },
+            {
+                _id: random(25),
+                name: "React",
+                url: "../categories/react/questions",
+                totalCount: 10,
+                todaysCount: 0,
+                weekCount: 0
+            },
+            {
+                _id: random(25),
+                name: "JavaScript",
+                url: "../categories/javascript/questions",
+                totalCount: 100,
+                todaysCount: 1,
+                weekCount: 2
+            },
+            {
+                _id: random(25),
+                name: "HTML",
+                url: "../categories/html/questions",
+                totalCount: 100,
+                todaysCount: 10,
+                weekCount: 20
+            },
+            {
+                _id: random(25),
+                name: "CSS",
+                url: "../categories/css/questions",
+                todaysCount: 0,
+                weekCount: 0
+            },
+            {
+                _id: random(25),
+                name: "BootStrap",
+                url: "../categories/bootstrap/questions",
+                totalCount: 1000,
+                todaysCount: 4,
+                weekCount: 700
+            },
+            {
+                _id: random(25),
+                name: "SQL",
+                url: "../categories/sql/questions",
+                totalCount: 1000,
+                todaysCount: 4,
+                weekCount: 700
+            },
+            {
+                _id: random(25),
+                name: "MongoDB",
+                url: "../categories/mongodb/questions",
+                totalCount: 1000,
+                todaysCount: 4,
+                weekCount: 700
+            },
+            {
+                _id: random(25),
+                name: "C",
+                url: "../categories/c/questions",
+                totalCount: 1000,
+                todaysCount: 4,
+                weekCount: 700
+            },
+            {
+                _id: random(25),
+                name: "Web Programming",
+                url: "../categories/awp/questions",
+                totalCount: 1000,
+                todaysCount: 4,
+                weekCount: 700
+            },
+            
+        ]))
+    }
+}

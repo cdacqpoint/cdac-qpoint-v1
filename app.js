2b80c74aadb929c71d7e3c39ddecdc66b5e17d49
Sai Krishnan S <sai.saikrishnan@gmail.com>
2020-01-23 13:28:11 +0530

    added cors for react to work - xi

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var logger = require('morgan');
 
 //Compression & helmet
 var compression = require('compression');
- var helmet = require('helmet');
+var helmet = require('helmet');
+// For React to use
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(helmet());
+app.use(cors());
 app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 

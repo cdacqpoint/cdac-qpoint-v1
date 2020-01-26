ae50eba06278f5505e5c81f5bf983e18999c3fa0
Sai Krishnan S <sai.saikrishnan@gmail.com>
2020-01-26 14:02:57 +0530

    build code added -xi

---
+++ build/precache-manifest.fe908d47df284c15d3446248558a02ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "afbd0f94bf41d74cbf37",
+    "url": "./static/css/main.1123457a.chunk.css"
+  },
+  {
+    "revision": "afbd0f94bf41d74cbf37",
+    "url": "./static/js/main.afbd0f94.chunk.js"
+  },
+  {
+    "revision": "97b499aec392c285bb6e",
+    "url": "./static/css/1.1bd202f0.chunk.css"
+  },
+  {
+    "revision": "97b499aec392c285bb6e",
+    "url": "./static/js/1.97b499ae.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "./static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "bba3157bb67f6a1d64dbee60caf42603",
+    "url": "./index.html"
+  }
+];
